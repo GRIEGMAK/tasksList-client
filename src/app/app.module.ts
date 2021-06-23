1ce2dc6a7756529294a1f41e93434a271cf01fbb
GRIEGMAK <g.m.ak@yandex.ru>
2021-06-23 21:50:12 +0600

    сделал header без ссылок и загрузок только оформление

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HeaderComponent, ListComponent, TodosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, 
+    MatToolbarModule, 
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
